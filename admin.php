b46bd7c7b814478be3ed470b089369e92c919426
nazmiyakob <38222116+nazmiyakob@users.noreply.github.com>
2018-04-16 00:11:27 +0800

    add new file

---
+++ admin.php
@@ -0,0 +1,135 @@
+<html lang="en">
+<head>
+<title>E-Rented House</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script src="js/jquery-1.6.js" ></script>
+<script src="js/cufon-yui.js"></script>
+<script src="js/cufon-replace.js"></script>
+<script src="js/Didact_Gothic_400.font.js"></script>
+<script src="js/jquery.nivo.slider.pack.js"></script>
+<script src="js/atooltip.jquery.js"></script>
+<script src="js/jquery.jqtransform.js" ></script>
+<script src="js/script.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<style type="text/css">.bg{behavior:url("js/PIE.htc");}</style>
+<![endif]-->
+</head>
+<body id="page1">
+<div class="body1">
+  <div class="main">
+    <!-- header -->
+    <header>
+      <h1><a href="index.php" id="logo"></a></h1>
+      <div class="wrapper">
+        <ul id="icons">
+          <li><a href="#" class="normaltip"><img src="images/icon1.jpg" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon2.jpg" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon3.jpg" alt=""></a></li>
+        </ul>
+      </div>
+      <nav>
+        <ul id="menu">
+          <li class="end" id="menu_active"><a href="admin.php">Admin</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- / header -->
+  </div>
+</div>
+<!-- content -->
+<div class="body2">
+  <div class="main">
+    <section id="content">
+      <div class="wrapper">
+        <article>
+          <form id="form_1" action="#" method="post">
+            <div class="pad1">
+              <h3>Apporvement</h3>
+              <?php 
+
+                $username = "root";
+                $password = "";
+                $hostname = "localhost";
+
+
+                $con = mysql_connect($hostname, $username, $password) or die("Could not connect to database");
+
+                mysql_select_db("e_rented_house", $con); 
+                $query1=mysql_query("SELECT * FROM user");
+
+                echo " <h1><center> LIST USER </center></h1>
+                <p><br>
+                <table border='1' text-align='center' width='100%'> 
+                <tr bgcolor='lightyellow'>
+                  <th><center> FULL NAME </center></th>
+                  <th><center> IC NUMBER </center></th>
+                  <th><center> USERNAME </center></th>
+                  <th><center> ACTION </center></th>
+                </tr>
+                </div></div>";
+
+                while($query2=mysql_fetch_array($query1))
+                {
+                  echo "";
+                  echo"<tr height='30px'>";
+                  echo "<td align='center' valign='top'>&nbsp;" . $query2['fullname'] . "</td>";
+                  echo "<td align='center' valign='top'>&nbsp;" . $query2['ic_number'] . "</td>";
+                  echo "<td align='center' valign='top'>&nbsp;" . $query2['username'] . "</td>";
+                  echo "<td align='center'>&nbsp;" . "<form action='' method='POST'><button type='submit' span='2' formaction='user_delete.php?username=".$query2['username']."'>DELETE</button></form>";
+                  echo "</td>";
+                  echo "</tr>";
+                }
+
+              echo "</table>";
+
+              ?>
+            </div>
+          </form>
+        </article>
+      </div>
+    </section>
+  </div>
+</div>
+<script>Cufon.now();</script>
+<script>
+$(window).load(function () {
+    $('#slider').nivoSlider({
+        effect: 'sliceUpDown', //Specify sets like: 'fold,fade,sliceDown, sliceDownLeft, sliceUp, sliceUpLeft, sliceUpDown, sliceUpDownLeft'
+        slices: 17,
+        animSpeed: 500,
+        pauseTime: 6000,
+        startSlide: 0, //Set starting Slide (0 index)
+        directionNav: false, //Next & Prev
+        directionNavHide: false, //Only show on hover
+        controlNav: true, //1,2,3...
+        controlNavThumbs: false, //Use thumbnails for Control Nav
+        controlNavThumbsFromRel: false, //Use image rel for thumbs
+        controlNavThumbsSearch: '.jpg', //Replace this with...
+        controlNavThumbsReplace: '_thumb.jpg', //...this in thumb Image src
+        keyboardNav: true, //Use left & right arrows
+        pauseOnHover: true, //Stop animation while hovering
+        manualAdvance: false, //Force manual transitions
+        captionOpacity: 1, //Universal caption opacity
+        beforeChange: function () {
+            $('.nivo-caption').animate({
+                bottom: '-110'
+            }, 400, 'easeInBack')
+        },
+        afterChange: function () {
+            Cufon.refresh();
+            $('.nivo-caption').animate({
+                bottom: '-20'
+            }, 400, 'easeOutBack')
+        },
+        slideshowEnd: function () {} //Triggers after all slides have been shown
+    });
+    Cufon.refresh();
+});
+</script>
+</body>
+</html>
