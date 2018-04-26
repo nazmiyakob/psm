621980867e85381726bce5a267f3dec902f11fb5
nazmiyakob <nazmiyakob94@gmail.com>
2018-04-27 00:38:22 +0800

    update sql

--- e_rented_house.sql
+++ e_rented_house.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2018 at 09:35 AM
+-- Generation Time: Apr 26, 2018 at 06:37 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 5.6.32
 
@@ -48404,17 +48404,18 @@ CREATE TABLE `residential` (
   `residential_year` int(4) NOT NULL,
   `furnishing` varchar(50) NOT NULL,
   `res_state` varchar(50) NOT NULL,
-  `res_cities` varchar(50) NOT NULL
+  `res_cities` varchar(50) NOT NULL,
+  `status` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `residential`
 --
 
-INSERT INTO `residential` (`residential_id`, `residential_type`, `residential_price`, `residential_img`, `residential_description`, `residential_year`, `furnishing`, `res_state`, `res_cities`) VALUES
-(1, 'Apartment', 20000, '', 'test', 2003, 'Partially Furnishing', 'Melaka', 'Durian Tunggal'),
-(2, '2-Sty Terrace', 2000, '', 'test2', 2003, 'Not Furnishing', 'Negeri Sembilan', 'Batu Berendam'),
-(3, '1-Sty Terrace', 23000, '', 'aaa', 2000, 'Partially Furnishing', 'Johor', 'Batu Berendam');
+INSERT INTO `residential` (`residential_id`, `residential_type`, `residential_price`, `residential_img`, `residential_description`, `residential_year`, `furnishing`, `res_state`, `res_cities`, `status`) VALUES
+(1, 'Apartment', 20000, '', 'test', 2003, 'Partially Furnishing', 'Melaka', 'Durian Tunggal', ''),
+(2, '2-Sty Terrace', 2000, '', 'test2', 2003, 'Not Furnishing', 'Negeri Sembilan', 'Batu Berendam', ''),
+(3, '1-Sty Terrace', 23000, '', 'aaa', 2000, 'Partially Furnishing', 'Johor', 'Batu Berendam', '');
 
 -- --------------------------------------------------------
 
@@ -52562,12 +52563,20 @@ INSERT INTO `states` (`id`, `name`, `country_id`) VALUES
 --
 
 CREATE TABLE `user` (
-  `ic_no` int(12) NOT NULL,
+  `ic_no` bigint(20) NOT NULL,
   `fullname` varchar(100) NOT NULL,
   `password` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `phone_number` int(12) NOT NULL
+  `phone_number` int(12) NOT NULL,
+  `user_type` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ic_no`, `fullname`, `password`, `email`, `phone_number`, `user_type`) VALUES
+(941016016117, 'Mohd Nazmi Syarifuddin Bin Mohd Yakob', 'abc123', 'nazmiyakob1994@gmail.com', 136689218, 'Admin');
 
 --
 -- Indexes for dumped tables
