f110cefe6673b5be8441f02c49cac08f1b18d847
nazmiyakob <38222116+nazmiyakob@users.noreply.github.com>
2018-05-14 04:42:49 +0800

    graph

--- report.php
+++ report.php
@@ -14,12 +14,23 @@
 <script src="js/jquery.jqtransform.js" ></script>
 <script src="js/script.js"></script>
 <script src="js/jquery.easing.1.3.js"></script>
+<script src="js/Chart.js"></script>
 <!--[if lt IE 9]>
 <script src="js/html5.js"></script>
 <style type="text/css">.bg{behavior:url("js/PIE.htc");}</style>
 <![endif]aaaaaaaaaaaaa-->
 </head>
 <body id="page1">
+<?php 
+  $username = "root";
+  $password = "";
+  $hostname = "localhost";
+
+
+  $con = mysql_connect($hostname, $username, $password) or die("Could not connect to database");
+
+  mysql_select_db("e_rented_house", $con); 
+?>
 <div class="body1">
   <div class="main">
     <!-- header -->
@@ -34,8 +45,9 @@
       </div>
       <nav>
         <ul id="menu">
-          <li id="menu_active"><a href="admin.php">Home</a></li>
+          <li><a href="admin.php">Home</a></li>
           <li class="menu_active"><a href="approvement.php">Approvement</a></li>
+          <li class="menu_active" id="menu_active"><a href="report.php">Report</a></li>
           <li class="endr" ><a href="logout.php">LogOut</a></li>
         </ul>
       </nav>
@@ -48,49 +60,60 @@
   <div class="main">
     <section id="content">
       <div class="wrapper" style="height: 100%;">
-        <article>
-          <form id="form_1" action="#" method="post">
-            <div class="pad1">
-              <h3>Approvement</h3>
-              <?php
-                mysql_connect("localhost","root","");
-                mysql_select_db("e_rented_house");
-                
-                $query1=mysql_query("SELECT * FROM user");
+        
+          
+            <div class="pad1" >
+              <canvas id="myChart" width="700" height="300"></canvas>
+              <?php 
+              $sql1 = mysql_query("SELECT count(booking_id) from booking join residential on booking.residential_id = residential.residential_id where res_state ='Melaka'");
+              $query1=mysql_fetch_array($sql1);
 
-                echo " <h4><center> LIST USER </center></h4>
-                <p><br>
-                <table border='1' text-align='center' width='100%'> 
-                <tr bgcolor='lightyellow'>
-                  <th><center> FULL NAME </center></th>
-                  <th><center> IC NUMBER </center></th>
-                  <th><center> EMAIL </center></th>
-                  <th><center> ACTION </center></th>
-                </tr>
-                </div></div>";
+              $sql2 = mysql_query("SELECT count(booking_id) from booking join residential on booking.residential_id = residential.residential_id where res_state ='Johor'");
+              $query2=mysql_fetch_array($sql2);
 
-                while($query2=mysql_fetch_array($query1))
-                {
-                  echo "";
-                  echo"<tr height='30px'>";
-                  echo "<td align='center' valign='top'>&nbsp;" . $query2['fullname'] . "</td>";
-                  echo "<td align='center' valign='top'>&nbsp;" . $query2['ic_no'] . "</td>";
-                  echo "<td align='center' valign='top'>&nbsp;" . $query2['email'] . "</td>";
-                  echo "<td align='center'>&nbsp;" . "<form action='' method='POST'><button type='submit' class='button' span='2' formaction='user_delete.php?fullname=".$query2['fullname']."'>DELETE</button></form>";
-                  echo "</td>";
-                  echo "</tr>";
-                }
-
-              echo "</table>";
+              $sql3 = mysql_query("SELECT count(booking_id) from booking join residential on booking.residential_id = residential.residential_id where res_state ='Negeri Sembilan'");
+              $query3=mysql_fetch_array($sql3);
+  
 
               ?>
+              
             </div>
-          </form>
-        </article>
+          
+        
       </div>
     </section>
   </div>
 </div>
+<script>
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ["Melaka","Johor","Negeri Sembilan","MN","MB","M","NC","PS","PT","PR","RAM","SC","SW","SR"],
+            datasets: [{
+              label: 'No of Booking',
+              data: [<?php echo $query1[0];?>,<?php echo $query2[0];?>,<?php echo $query3[0];?>
+                
+                ],
+              backgroundColor: [
+                 "#00FFFF","#FFE4C4","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c","#e74c3c",
+                 
+              ],
+              borderWidth: 2
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero:true
+                  
+                }
+              }]
+            }
+          }
+        });
+      </script>
 <script>Cufon.now();</script>
 <script>
 $(window).load(function () {
